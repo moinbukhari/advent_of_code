bc23e27b150f97699675b8a46bea46f7b40994e7
moinbukhari <moinbukhari4321@gmail.com>
2023-12-26 15:20:57 +0000

    day13 part1 solved

---
+++ 2023/day13.ts
@@ -0,0 +1,69 @@
+import fs from "fs";
+const temp = `#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#`;
+
+function getTransposedArray(arr: string[][]): string[] {
+  const transposedArr: string[][] = [];
+  for (let i = 0; i < arr[0].length; i++) {
+    transposedArr[i] = [];
+    for (let j = 0; j < arr.length; j++) {
+      transposedArr[i][j] = arr[j][i];
+    }
+  }
+  return transposedArr.map((col) => col.join(""));
+}
+
+function findMirrorIndex(pattern: string[]): number {
+  for (let i = 1; i < pattern.length; i++) {
+    let above = pattern.slice(0, i);
+    let below = pattern.slice(i);
+    if (above.length <= below.length) {
+      below = below.slice(0, above.length);
+    } else {
+      above = above.slice(above.length - below.length);
+    }
+
+    if (above.join("") === below.reverse().join("")) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+const data = fs.readFileSync("day13.txt", "utf8");
+const patterns = data.split(/\n\n/).map((line) => {
+  return line.split(/\n/);
+});
+
+const scores: number[] = [];
+
+patterns.forEach((p) => {
+  const horizontalMirrorIndex = findMirrorIndex(p);
+  if (horizontalMirrorIndex !== -1) {
+    scores.push(horizontalMirrorIndex * 100);
+  } else {
+    const transposedArr = getTransposedArray(p.map((row) => row.split("")));
+    const verticalMirrorIndex = findMirrorIndex(transposedArr);
+    if (verticalMirrorIndex !== -1) {
+      scores.push(verticalMirrorIndex);
+    } else {
+      scores.push(0);
+    }
+  }
+});
+
+console.log(scores.reduce((acc, curr) => acc + curr));
